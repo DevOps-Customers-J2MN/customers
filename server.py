036450e2ea87a1952c416cf0346e5c5e0aeb23c8
Meenakshi Sundaram Arumugam <msa503@nyu.edu>
2018-03-01 15:32:06 -0500

    Create Customer - Service #11

--- server.py
+++ server.py
@@ -1,10 +1,154 @@
-from flask import Flask
+"""
+Customer Service
+This is an example of a customer service written with Python Flask
+It demonstraits how a RESTful service should be implemented.
+Paths
+-----
+GET  /customers - Retrieves a list of Customer from the database
+GET  /customers{id} - Retrirves a Customer with a specific id
+POST /customers - Creates a Customer in the datbase from the posted database
+PUT  /customers/{id} - Updates a Customer in the database fom the posted database
+DELETE /customers{id} - Removes a Customer from the database that matches the id
+"""
+
+import os
+import sys
+import logging
+from flask import Flask, Response, jsonify, request, json, url_for, make_response
+from models import Customer, DataValidationError
+
+# Pull options from environment
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+PORT = os.getenv('PORT', '5000')
+
+# Create Flask application
 app = Flask(__name__)
 
+# Status Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+HTTP_400_BAD_REQUEST = 400
+HTTP_404_NOT_FOUND = 404
+HTTP_409_CONFLICT = 409
 
+######################################################################
+# Error Handlers
+######################################################################
+@app.errorhandler(DataValidationError)
+def request_validation_error(error):
+    """ Handles all data validation issues from the model """
+    return bad_request(error)
+
+@app.errorhandler(400)
+def bad_request(error):
+    """ Handles requests that have bad or malformed data """
+    return jsonify(status=400, error='Bad Request', message=error.message), 400
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Handles Customers that cannot be found """
+    return jsonify(status=404, error='Not Found', message=error.message), 404
+
+@app.errorhandler(405)
+def method_not_supported(error):
+    """ Handles bad method calls """
+    return jsonify(status=405, error='Method not Allowed',
+                   message='Your request method is not supported.' \
+                   ' Check your HTTP method and try again.'), 405
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    """ Handles catostrophic errors """
+    return jsonify(status=500, error='Internal Server Error', message=error.message), 500
+
+
+######################################################################
+# GET INDEX
+######################################################################
 @app.route('/')
 def index():
-    return 'Hello Flask!'
+    """ Return something useful by default """
+    return jsonify(name='Customer REST API Service',
+                   version='1.0',
+                   url=url_for('list_customers', _external=True)), HTTP_200_OK
 
-if __name__ == '__main__':
-    app.run()
+######################################################################
+# LIST ALL CUSTOMERS
+######################################################################
+@app.route('/customers', methods=['GET'])
+def list_customers():
+    """ Retrieves a list of customers from the database """
+    results = []
+    results = Customer.all()
+
+    return jsonify([customer.serialize() for customer in results]), HTTP_200_OK
+
+######################################################################
+# RETRIEVE A CUSTOMER
+######################################################################
+@app.route('/customers/<int:id>', methods=['GET'])
+def get_customers(id):
+    """ Retrieves a Customer with a specific id """
+    customer = Customer.find(id)
+    if customer:
+        message = customer.serialize()
+        return_code = HTTP_200_OK
+    else:
+        message = {'error' : 'Customer with id: %s was not found' % str(id)}
+        return_code = HTTP_404_NOT_FOUND
+
+    return jsonify(message), return_code
+
+######################################################################
+# ADD A NEW CUSTOMER
+######################################################################
+@app.route('/customers', methods=['POST'])
+def create_customers():
+    """ Creates a Customer in the datbase from the posted database """
+    payload = request.get_json()
+    customer = Customer()
+    customer.deserialize(payload)
+    customer.save()
+    message = customer.serialize()
+    response = make_response(jsonify(message), HTTP_201_CREATED)
+    response.headers['Location'] = url_for('get_customers', id=customer.id, _external=True)
+    return response
+
+
+######################################################################
+#   U T I L I T Y   F U N C T I O N S
+######################################################################
+def initialize_logging(log_level=logging.INFO):
+    """ Initialized the default logging to STDOUT """
+    if not app.debug:
+        print 'Setting up logging...'
+        # Set up default logging for submodules to use STDOUT
+        # datefmt='%m/%d/%Y %I:%M:%S %p'
+        fmt = '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+        logging.basicConfig(stream=sys.stdout, level=log_level, format=fmt)
+        # Make a new log handler that uses STDOUT
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(logging.Formatter(fmt))
+        handler.setLevel(log_level)
+        # Remove the Flask default handlers and use our own
+        handler_list = list(app.logger.handlers)
+        for log_handler in handler_list:
+            app.logger.removeHandler(log_handler)
+        app.logger.addHandler(handler)
+        app.logger.setLevel(log_level)
+        app.logger.info('Logging handler established')
+
+
+######################################################################
+#   M A I N
+######################################################################
+if __name__ == "__main__":
+    print "*********************************"
+    print " C U S T O M E R  S E R V I C E "
+    print "*********************************"
+    initialize_logging()
+    # dummy data for testing
+    Customer(0, 'Meenakshi Sundaram', 'Sundaram, 'Jersey City', '2016604601, 'msa503@nyu.edu', '123456').save()
+    app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
+
